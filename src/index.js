dcf815b6f00b7457c5485dd55356935aa9987406
stupid <sunfeng32@qq.com>
2017-12-14 14:05:02 +0800

    feat(enzyme-shallow-until): init

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import React, {Component} from 'react'
+// Copied from gist
+// https://gist.github.com/matthieuprat/5fd37abbd4a4002e6cfe0c73ae54cda8
+function shallowRecursively(wrapper, selector) {
+  // Do not try to shallow render empty nodes and host elements
+  // (a.k.a primitives). Simply return the wrapper in that case.
+  if (wrapper.isEmptyRender() || typeof wrapper.getElement().type === 'string') { return wrapper; }
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
-  }
+  return selector && wrapper.is(selector)
+    ? wrapper
+    : shallowRecursively(wrapper.dive(), selector);
+}
+
+export default function until(selector) {
+  return this.single('until', () => shallowRecursively(this, selector));
 }
--- tests/index-test.js
+++ tests/index-test.js
@@ -1,23 +1,79 @@
-import expect from 'expect'
-import React from 'react'
-import {render, unmountComponentAtNode} from 'react-dom'
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import ShallowWrapper from 'enzyme/ShallowWrapper';
+import expect from 'expect';
 
-import Component from 'src/'
+import until from 'src';
 
-describe('Component', () => {
-  let node
+Enzyme.configure({ adapter: new Adapter() });
+ShallowWrapper.prototype.until = until;
 
-  beforeEach(() => {
-    node = document.createElement('div')
-  })
+describe('until', () => {
+  const Div = () => <div />;
+  const Foo = () => <Div />;
+  const hoc = Component => () => <Component />;
 
-  afterEach(() => {
-    unmountComponentAtNode(node)
-  })
+  it('shallow renders the current wrapper one level deep', () => {
+    const EnhancedFoo = hoc(Foo);
+    const wrapper = shallow(<EnhancedFoo />).until(Foo);
+    expect(wrapper.contains(<Foo />)).toBe(true);
+    expect(wrapper.dive().contains(<Div />)).toBe(true);
+  });
 
-  it('displays a welcome message', () => {
-    render(<Component/>, node, () => {
-      expect(node.innerHTML).toContain('Welcome to React components')
-    })
-  })
-})
+  it('shallow renders the current wrapper several levels deep', () => {
+    const EnhancedFoo = hoc(hoc(hoc(Foo)));
+    const wrapper = shallow(<EnhancedFoo />).until(Foo);
+    expect(wrapper.contains(<Foo />)).toBe(true);
+    expect(wrapper.dive().contains(<Div />)).toBe(true);
+  });
+
+  it('supports string selectors', () => {
+    const EnhancedFoo = hoc(Foo);
+    const wrapper = shallow(<EnhancedFoo />).until('Foo');
+    expect(wrapper.contains(<Foo />)).toBe(true);
+    expect(wrapper.dive().contains(<Div />)).toBe(true);
+  });
+
+  it('shallow renders as much as possible when no selector is provided', () => {
+    const EnhancedFoo = hoc(hoc(Foo));
+    const wrapper = shallow(<EnhancedFoo />).until();
+    expect(wrapper.contains(<div />)).toBe(true);
+  });
+
+  it('stops shallow rendering when the wrapper is empty', () => {
+    const NullComponent = () => null;
+    const wrapper = shallow(<NullComponent />).until();
+    expect(wrapper.isEmptyRender()).toBe(true);
+  });
+
+  it('shallow renders the current wrapper even if the selector never matches', () => {
+    const EnhancedFoo = hoc(Foo);
+    const wrapper = shallow(<EnhancedFoo />).until('Whatever');
+    expect(wrapper.contains(<div />)).toBe(true);
+  });
+
+  it('stops shallow rendering when it encounters a DOM element', () => {
+    const Container = () => <div><Foo /></div>;
+
+    let wrapper = shallow(<Container />).until(Foo);
+    expect(wrapper.contains(<div><Foo /></div>)).toBe(true);
+
+    const EnhancedContainer = hoc(hoc(Container));
+    wrapper = shallow(<EnhancedContainer />).until(Foo);
+    expect(wrapper.contains(<div><Foo /></div>)).toBe(true);
+  });
+
+  it('throws when it is called on an empty wrapper', () => {
+    expect(() => shallow(<Foo />).find('Whatever').until()).toThrow(
+      Error,
+      'Method “until” is only meant to be run on a single node. 0 found instead.',
+    );
+  });
+
+  it('shallow renders non-root wrappers', () => {
+    const Container = () => <div><Foo /></div>;
+    const wrapper = shallow(<Container />).find(Foo).until();
+    expect(wrapper.contains(<div />)).toBe(true);
+  });
+});
